f79492b1f9b81b1abc6a646a643686d2a21c48f1
ddamar798 <damarbudiprasetyo714@gmail.com>
2024-11-15 18:39:57 +0700

    test

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog"></a>
+    <a href="/contact"></a>
+    
+    <article>
+        <h3>KONTEN 1</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, dolor repellendus odit a repellat reprehenderit corrupti perspiciatis facilis nisi doloribus, ut asperiores. Animi architecto minima porro rerum odit soluta asperiores?Z</p>
+    </article>
+
+    <article>
+        <h3>KONTEN 2</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem in, sit eligendi quisquam similique iste ex, optio, quis quasi hic cum blanditiis. Quod sint quidem, consectetur illum eveniet nemo voluptates?</p>
+    </article>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog"></a>
+    <a href="/contact"></a>
+    
+    <h1>Call Me</h1>
+    <button><a href="https://laravel.com/docs/11.x">Contact Me!</a> </button>
+</body>
+</html>
